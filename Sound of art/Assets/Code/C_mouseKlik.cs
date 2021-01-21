23ef6938f899317b424980db39f810bf7d6dc426
Mijs <mijs.tenhove@hotmail.com>
2021-01-21 19:24:37 +0100

    unity

---
+++ Sound of art/Assets/Code/C_mouseKlik.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_mouseKlik : MonoBehaviour
+{
+    AudioSource artSound;
+    
+    void Start()
+    {
+        artSound = GetComponent<AudioSource>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            PrintName(hit.transform.gameObject);
+        }
+        
+    }
+
+    void PrintName(GameObject go) 
+    {
+        print(go.name);
+        if (Input.GetMouseButtonUp(0)&& go.name == gameObject.name)
+            {
+            artSound.Play();
+        }
+    } 
+}
+
---
+++ Sound of art/Assets/Code/Playmusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playmusic : MonoBehaviour
+{
+     AudioSource artSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        artSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.tag == "player")
+        {
+            Debug.Log("dfg");
+            artSound.Play();
+
+        }
+
+    }
+}
