34e52479c2071229ac319fde6e9df6e6d6afa932
Wouter M. Besse <wouterbesse@gmail.com>
2021-01-26 01:12:17 +0100

    Updated the art scene, added pebbles etc. :)

---
+++ Sound of art/Assets/StoneSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip[] AudioList;
+    static AudioSource audioSrc;
+
+
+    void Start()
+    {
+        AudioList = new AudioClip[]{(AudioClip)Resources.Load("Stone1"),
+                                     (AudioClip)Resources.Load("Stone2"),
+                                     (AudioClip)Resources.Load("Stone3"),
+                                     (AudioClip)Resources.Load("Stone4"),
+                                     (AudioClip)Resources.Load("Stone5")};
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider collision)
+    {
+        Debug.Log("KAAS!");
+        AudioClip r = AudioList[Random.Range(0, AudioList.Length)];
+        audioSrc.PlayOneShot(r, 0.3F);
+    }
+}
--- Sound of art/Assets/Wouter/ParticleAudio.cs
+++ Sound of art/Assets/Wouter/ParticleAudio.cs
@@ -6,11 +6,11 @@ public class ParticleAudio : MonoBehaviour
 {
 
     public ParticleSystem ParticleAudios;
-    public static AudioClip PS1;
-    public static AudioClip PS2;
-    public static AudioClip PS3;
-    public static AudioClip PS4;
-    public static AudioClip PS5;
+    //public static AudioClip PS1;
+    //public static AudioClip PS2;
+    //public static AudioClip PS3;
+    //public static AudioClip PS4;
+    //public static AudioClip PS5;
     public AudioClip[] AudioList;
     //public ArrayList[] audiofiles = new AudioList[] { PS1, PS2, PS3, PS4, PS5 };
     static AudioSource audioSrc;
@@ -35,11 +35,11 @@ public class ParticleAudio : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log("KAAS!");
-        Debug.Log(AudioList);
+        //Debug.Log("KAAS!");
+        //Debug.Log(AudioList);
         //audioSrc.PlayOneShot(PS1, 0.3F);
         AudioClip r = AudioList[Random.Range(0, AudioList.Length)];
-        Debug.Log(r);
+        //Debug.Log(r);
         audioSrc.PlayOneShot(r, 0.3F);
         //audioSrc.Stop();
     }
